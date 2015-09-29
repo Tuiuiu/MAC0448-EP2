99f550ba3ae9d8219b1965ca5662ece449e3aa7a
Lucas Dário <lucasdario.dariolucas@gmail.com>
2015-09-29 17:33:29 -0300

    Inseridos novos atributos e metodos à classe Usuário
    O usuário agora guarda a informação se está em jogo ou não e se está
    conectado ou não. Alem disso, a cada novo login, podemos alterar o
    estado da conexão do Usuário.
    
    Signed off by: @gabrielguilhoto

--- Usuario.cpp
+++ Usuario.cpp
@@ -3,3 +3,32 @@
 void Usuario::escreve(std::string mensagem) {
 	conexao->enviaMensagem(mensagem);
 }
+
+void Usuario::atualizaConexao(Conexao *novaConexao) {
+	free (conexao);
+	conexao = novaConexao;
+}
+
+void Usuario::conecta() {
+	conectado = true;
+}
+
+void Usuario::desconecta() {
+	conectado = false;
+}
+
+void Usuario::entra_jogo() {
+	em_jogo = true;
+}
+
+void Usuario::sai_jogo() {
+	em_jogo = false;
+}
+
+std::string Usuario::get_login() {
+	return login;
+}
+
+bool Usuario::confere_senha(std::string senha_arg) {
+	return senha_arg == senha;
+}
--- Usuario.hpp
+++ Usuario.hpp
@@ -7,12 +7,25 @@
 
 class Usuario {
   public:
-  	Usuario(Conexao *conexao) : conexao(conexao) {}
+  	Usuario(Conexao *conexao, std::string login_arg, std::string senha_arg) 
+  		   : conexao(conexao),conectado(true),em_jogo(false) 
+  		   { login = login_arg; senha = senha_arg; }
   	void escreve(std::string mensagem);
+  	void atualizaConexao(Conexao *novaConexao);
+  	bool esta_conectado();
+  	bool esta_em_jogo();
+  	void conecta();
+  	void desconecta();
+  	void entra_jogo();
+  	void sai_jogo();
+  	std::string get_login();
+  	bool confere_senha(std::string senha_arg);
   private:
   	Conexao *conexao;
   	std::string login;
   	std::string senha;
+  	bool conectado;
+  	bool em_jogo;
 };
 
 #endif // _USUARIO_HPP
